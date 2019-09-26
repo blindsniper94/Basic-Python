7d0f0e2292a2b977aba61f59916e48c1ceb98454
Alex Bainter <shreder108@gmail.com>
2019-09-26 11:02:51 -0600

    Update drill.py
    Re-do of Drill 103 of Python

--- drill.py
+++ drill.py
@@ -1,3 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect('drill.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_files ( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+        col_filename TEXT)")
+    conn.commit()
+conn.close()
 
 
 import sqlite3
@@ -13,32 +24,19 @@ with conn:
     conn.commit()
 conn.close()
 
+
 conn = sqlite3.connect('drill.db')
 
-with conn:
-    cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_files( col_filename,col_filetype ) VALUES(?,?)",\
-                ('information','.docx'))
-    cur.execute("INSERT INTO tbl_files( col_filename,col_filetype ) VALUES(?,?)",\
-                ('Hello','.txt'))
-    cur.execute("INSERT INTO tbl_files( col_filename,col_filetype ) VALUES(?,?)",\
-                ('myImage','.png'))
-    cur.execute("INSERT INTO tbl_files( col_filename,col_filetype ) VALUES(?,?)",\
-                ('myMovie','.mpg'))
-    cur.execute("INSERT INTO tbl_files( col_filename,col_filetype ) VALUES(?,?)",\
-                ('World','.txt'))
-    cur.execute("INSERT INTO tbl_files( col_filename,col_filetype ) VALUES(?,?)",\
-                ('data','.pdf'))
-    cur.execute("INSERT INTO tbl_files( col_filename,col_filetype ) VALUES(?,?)",\
-                ('myPhoto','.jpg'))
-    conn.commit()
-conn.close
-        
-conn = sqlite3.connect('drill.db')
-with conn:
-    cur = conn.cursor()
-    cur.execute("SELECT col_filename, col_filetype FROM tbl_files WHERE col_filetype = '.txt'")
-    varFile = cur.fetchall()
-    for item in varFile:
-        msg = "File Name: {} \nFile Type: {}".format(item[0],item[1])
-        print(msg)
+fileList = ('information.docx','Hello.txt','myImage.png', \
+            'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
+
+for file in fileList:
+    if file.endswith(".txt"):
+        with conn:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO tbl_files( col_filename ) VALUES (?)", (file,))
+            conn.commit()
+conn.close()
+
+
+
